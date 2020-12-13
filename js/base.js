644f41f7cfda76948e1fc8bfeb2e5a2e78346756
Sang Pham <psang242001@gmail.com>
2020-12-13 11:58:32 +0700

    new car update

--- js/base.js
+++ js/base.js
@@ -5,6 +5,7 @@ $(window).on("load", function () {
 
 function enableScroll() {
     // document.querySelector("body").style.overflowY = "auto";
+    $("body").removeClass("stop-scrolling");
     $("body").addClass("enable-scrolling");
 }
 
